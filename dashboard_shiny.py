cfd5354e2add1c4a41be57d8a18fa4a0560b4057
m-fariz-a <falasyir@yahoo.com>
2024-04-20 07:37:52 +0700

    feat: add pivot table

--- dashboard_shiny.py
+++ dashboard_shiny.py
@@ -1,10 +1,30 @@
 from libs.create_data import create_data
 
+import pandas as pd
 import plotly.express as px
 from shiny.express import input, render, ui
 from shinywidgets import render_widget, render_plotly
 
 df_data = create_data()
+cell_hover = {  # for row hover use <tr> instead of <td>
+    'selector': 'tr:hover',
+    'props': 'background-color: rgba(198, 198, 198, 0.18)'
+}
+# index_names = {
+#     'selector': '.index_name',
+#     'props': 'font-style: italic; background-color: rgba(0, 0, 0, 0.61); color: darkgrey; font-weight:normal;'
+# }
+# headers = {
+#     'selector': 'th:not(.index_name)',
+#     'props': 'background-color: rgba(179, 179, 179, 0.61);'
+# }
+cell_styling = [
+    {'selector': 'tbody tr:nth-child(odd)', 'props': 'background-color : rgba(230, 230, 230, 0.25)'},
+    {'selector': 'tbody tr:nth-child(even)', 'props': 'background-color : rgba(200, 200, 200, 0.25)'},
+    cell_hover
+]
+
+
 
 
 ui.page_opts(title="Example Data Display")
@@ -17,12 +37,12 @@ ui.markdown(
 
 
 with ui.layout_columns(
-    col_widths={"sm": (7, 5, 12)},
+    col_widths={"sm": (7, 5)},
     # row_heights=(2, 3),
     # height="700px",
     # when the column width exceeds the screen width, the column will be wrapped to the next row
 ):
-    with ui.card(full_screen=False):
+    with ui.card(full_screen=True):
         ui.card_header("All Data")
         ui.markdown("This is the original data")
 
@@ -33,7 +53,7 @@ with ui.layout_columns(
                 df, filters=True, selection_mode='rows'
             )
 
-    with ui.card(full_screen=False):
+    with ui.card(full_screen=True):
         ui.card_header("Summary")
         ui.markdown("This is the summary of the data")
 
@@ -45,10 +65,76 @@ with ui.layout_columns(
 
 ui.markdown("## Other Analysis")
 
-with ui.card(full_screen=False):
+with ui.card(full_screen=True):
     ui.card_header("Scatter Plot of Age vs City")
 
     @render_plotly
     def scatter_plot():
         fig = px.scatter(df_data, x='City', y='Age', color='Gender', title='Age vs City')
         return fig
+
+ui.markdown("### Pivot v1")
+with ui.layout_columns(
+    col_widths={"sm": (6, 6)},
+    # row_heights=(2, 2),
+    height="400px",
+    # when the column width exceeds the screen width, the column will be wrapped to the next row
+):
+
+    with ui.card(full_screen=True):
+        ui.card_header("Pivot a")
+
+        @render.table
+        def pivot1a():
+            df = pd.pivot_table(df_data, index=['Gender', 'City'], columns='Married', values='Age', aggfunc='mean')
+            return df.reset_index()
+
+    with ui.card(full_screen=True):
+        ui.card_header("Pivot b")
+
+        @render.table
+        def pivot2a():
+            df = pd.pivot_table(df_data, index='Gender', columns=['City', 'Married'], values='Age', aggfunc='mean')
+            return df.reset_index()
+
+ui.markdown("### Pivot v2")
+with ui.layout_columns(
+    col_widths={"sm": (6, 6)},
+    # row_heights=(2, 2),
+    height="400px",
+    # when the column width exceeds the screen width, the column will be wrapped to the next row
+):
+
+    with ui.card(full_screen=True):
+        ui.card_header("Pivot 1")
+
+        @render.table
+        def pivot1b():
+            df = (
+                pd.pivot_table(df_data, index=['Gender', 'City'], columns='Married', values='Age', aggfunc='mean')
+            )
+            return (
+                df.style
+                .format(precision=2)
+                .set_table_styles(cell_styling)
+                .set_sticky(axis='columns')
+                # .set_sticky(axis='columns')
+
+            )
+
+    with ui.card(full_screen=True):
+        ui.card_header("Pivot 2")
+
+        @render.table
+        def pivot2b():
+            df = (
+                pd.pivot_table(df_data, index='Gender', columns=['City', 'Married'], values='Age', aggfunc='mean')
+            )
+            return (
+                df.style
+                .format(precision=2)
+                .set_table_styles(cell_styling)
+                .set_sticky(axis='index')
+                # .set_sticky(axis='columns')
+
+            )
